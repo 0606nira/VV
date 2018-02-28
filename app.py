81ba40196ea89bcc13e4215bae9c3c7f1d632ddb
0606nira <nirachorn66@gmail.com>
2018-02-28 17:04:25 +0700

    make it better

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+
 from linebot import (
     LineBotApi, WebhookHandler,
 )
@@ -6,13 +7,13 @@ from linebot.exceptions import (
     InvalidSignatureError, LineBotApiError,
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('MEMIUEV7R2dzmxXVTkQRcgply61mFF16A/BEXFbh01XuuN1oGwhLH5t+GbxcJRIXEsiioQe7xhs0mluGITwfR55jRSRsd3R+JTBz6Z1O3Q+Ei0OIYS2QT0Mg86n6UZowtp0nO7HWmJBQJoCc4nSyMgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('13c1dcf5fa5fe8495b15f1ab271791f5')
+handler = linebot.WebhookHandler('13c1dcf5fa5fe8495b15f1ab271791f5')
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -34,9 +35,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
-      line_bot_api.reply_message(
-          event.reply_token,
-          TextSendMessage(text="Hello VV"))
+	line_bot_api.reply_message(
+		event.reply_token,
+		TextSendMessage(text="Hello VV"))
+	except linebot.exceptions.LineBotApiError as e:
+    		print(e.status_code)
+    		print(e.error.message)
+    		print(e.error.details)
 
 
 if __name__ == "__main__":
