306ff1cb7a996da218acfa503a9fb736842a77c7
0606nira <nirachorn66@gmail.com>
2018-03-15 21:51:44 +0700

    V

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi('MEMIUEV7R2dzmxXVTkQRcgply61mFF16A/BEXFbh01XuuN1oGwhLH5t+GbxcJRIXEsiioQe7xhs0mluGITwfR55jRSRsd3R+JTBz6Z1O3Q+Ei0OIYS2QT0Mg86n6UZowtp0nO7HWmJBQJoCc4nSyMgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('13c1dcf5fa5fe8495b15f1ab271791f5')
-#to = 'U5db26ce3aad1c4d83691ea5d6992116a'
+to = 'U5db26ce3aad1c4d83691ea5d6992116a'
 
 # username of anto.io account
 user = 'Nira'
@@ -42,6 +42,7 @@ def connectedCB():
 
 @app.route("/callback", methods=['POST'])
 def callback():
+	
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
@@ -58,11 +59,20 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-		line_bot_api.push_message(
-			event.to, 
-			TextSendMessage(text='Ayo'))
-			
+def dataCB(channel, msg):
+	if(channel == 'LED1'):
+		value = int(msg)
+		if(value == 1):
+			line_bot_api.push_message(to, TextSendMessage(text='LED1 On'))
+		else:
+			line_bot_api.push_message(to, TextSendMessage(text='LED1 Off'))
+	elif(channel == 'LED2'):
+		value = int(msg)
+		if(value == 1):
+			line_bot_api.push_message(to, TextSendMessage(text='LED2 On'))
+		else:
+			line_bot_api.push_message(to, TextSendMessage(text='LED2 Off'))
+		
    
 if __name__ == "__main__":
 
