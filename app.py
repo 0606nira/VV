a9de5ae73b9c9923d3dd9115873001e64309eeff
0606nira <nirachorn66@gmail.com>
2018-03-17 22:03:53 +0700

    V

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	message = event.message.text
+	#message = event.message.text
 	try:
 		if(message = event.message.text):
 			line_bot_api.reply_message(
