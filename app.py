340ec0800650609da5f3c8885fd80612c51726de
0606nira <nirachorn66@gmail.com>
2018-04-03 21:41:46 +0700

    'v'

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time, sys
 import json
 import send
 import requests 
-import http.client, urllib
+#import http.client, urllib
 from linebot import (
     LineBotApi, WebhookHandler,
 )
@@ -26,9 +26,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('MEMIUEV7R2dzmxXVTkQRcgply61mFF16A/BEXFbh01XuuN1oGwhLH5t+GbxcJRIXEsiioQe7xhs0mluGITwfR55jRSRsd3R+JTBz6Z1O3Q+Ei0OIYS2QT0Mg86n6UZowtp0nO7HWmJBQJoCc4nSyMgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('13c1dcf5fa5fe8495b15f1ab271791f5')
 
-API_KEY_WRITE = 'WWRZDTPBUN0O18FM'
-API_KEY_READ = 'ZDDJL90IXYJOIQ3S'
-CHANNEL_ID = '455279'
+
 
 timeis = time.localtime()
 time = time.strftime('%A %d %B %Y, %H:%M:%S', timeis) # กำหนดรูปแบบเวลา
--- send.py
+++ send.py
@@ -1,7 +1,36 @@
-def send_values(light):
+import http.client, urllib
+import time, sys
+
+API_KEY_WRITE = 'WWRZDTPBUN0O18FM'
+API_KEY_READ = 'ZDDJL90IXYJOIQ3S'
+CHANNEL_ID = '455279'
+
+def send_values1(light):
     global API_KEY_WRITE
     params = urllib.parse.urlencode(
              {'field1': light,
+              'key': API_KEY_WRITE} )
+    headers = { "Content-Type": "application/x-www-form-urlencoded",
+                "Accept": "text/plain" }
+    conn = http.client.HTTPConnection("api.thingspeak.com:80")
+    try:
+        conn.request( "POST", "/update", params, headers ) # send HTTP request
+        resp = conn.getresponse() # get HTTP response
+        print ('status:', resp.status, resp.reason) # read HTTP status
+        entry_id = resp.read()  # read response string
+        conn.close()            # close HTTP connection
+        if entry_id.isdigit() and int(entry_id) > 0:
+            print ('Entry ID:', entry_id)
+            return True
+        else:
+            return False
+    except:
+        print ("connection failed", sys.exc_info())
+		
+def send_values2(light):
+    global API_KEY_WRITE
+    params = urllib.parse.urlencode(
+             {'field2': light,
               'key': API_KEY_WRITE} )
     headers = { "Content-Type": "application/x-www-form-urlencoded",
                 "Accept": "text/plain" }
