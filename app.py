aa790871372263ec520fc1900a5268f80c67f9c1
0606nira <nirachorn66@gmail.com>
2018-04-04 15:52:43 +0700

    'v'

--- app.py
+++ app.py
@@ -105,6 +105,24 @@ def handle_message(event):
 			event.reply_token,
 			TextSendMessage(text="I don't know %s" %event.message.text))
 			
+def notification(event):
+    while True:
+        global API_KEY_READ
+        url = 'https://api.thingspeak.com/channels/455279/feeds.json?api_key=ZDDJL90IXYJOIQ3S&results=2'
+        response = urllib.request.urlopen(url)
+        data = json.load(response)
+        status = data['feeds'][0]['field1']
+        sta = data['feeds'][1]['field1']
+        if (status == 1):
+            line_bot_api.push_message(
+			event.source.user_id or event.source.group_id or event.source.room_id,
+			TextSendMessage(text='Light On'))
+            print('on')
+        else:
+            print ('error')
+        print (status)
+        print (sta)
+        time.sleep(5)			
 	
 
 image_carousel_template_message1 = TemplateSendMessage(
@@ -272,27 +290,8 @@ buttons_template_message4 = TemplateSendMessage(
             )
         ]
     )
-)
+)	
 
-handler.add(MessageEvent)		
-def notification(event):
-    while True:
-        global API_KEY_READ
-        url = 'https://api.thingspeak.com/channels/455279/feeds.json?api_key=ZDDJL90IXYJOIQ3S&results=2'
-        response = urllib.request.urlopen(url)
-        data = json.load(response)
-        status = data['feeds'][0]['field1']
-        sta = data['feeds'][1]['field1']
-        if (status == 1):
-            line_bot_api.push_message(
-			event.source.user_id or event.source.group_id or event.source.room_id,
-			TextSendMessage(text='Light On'))
-            print('on')
-        else:
-            print ('error')
-        print (status)
-        print (sta)
-        time.sleep(5)
 			
 if __name__ == "__main__":
     app.run(debug=True)
