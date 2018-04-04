fc6387efc4c5e369655f8448e2f6593e17254431
0606nira <nirachorn66@gmail.com>
2018-04-04 16:53:02 +0700

    'v'

--- noti.py
+++ noti.py
@@ -5,11 +5,11 @@ import http.client, urllib
 	
 def notification():
 	while True:
-        url = 'https://api.thingspeak.com/channels/455279/feeds.json?api_key=ZDDJL90IXYJOIQ3S&results=2'
-        response = urllib.request.urlopen(url)
-        data = json.load(response)
-        status = data['feeds'][0]['field1']
-        sta = data['feeds'][1]['field1']
+		url = 'https://api.thingspeak.com/channels/455279/feeds.json?api_key=ZDDJL90IXYJOIQ3S&results=2'
+		response = urllib.request.urlopen(url)
+		data = json.load(response)
+		status = data['feeds'][0]['field1']
+		sta = data['feeds'][1]['field1']
 		if(status == 1):
 			line_bot_api.push_message(
 				event.source.user_id or event.source.group_id or event.source.room_id, 
