8c27affac6de4c9da945ce93dd5dfb3402b42138
0606nira <nirachorn66@gmail.com>
2018-04-04 16:48:44 +0700

    'v'

--- noti.py
+++ noti.py
@@ -2,14 +2,9 @@ import time, sys
 import json
 import requests 
 import http.client, urllib
-
-API_KEY_WRITE = 'WWRZDTPBUN0O18FM'
-API_KEY_READ = 'ZDDJL90IXYJOIQ3S'
-CHANNEL_ID = '455279'
 	
 def notification():
 	while True:
-        global API_KEY_READ
         url = 'https://api.thingspeak.com/channels/455279/feeds.json?api_key=ZDDJL90IXYJOIQ3S&results=2'
         response = urllib.request.urlopen(url)
         data = json.load(response)
