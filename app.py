35d996380703b3ffa2229fa7cfffe455513d28de
0606nira <nirachorn66@gmail.com>
2018-04-08 15:00:31 +0700

    'v'

--- app.py
+++ app.py
@@ -133,9 +133,7 @@ def handle_message(event):
 		line_bot_api.reply_message(
 			event.reply_token,
 			TextSendMessage(text="I don't know %s" %event.message.text))
-
-@handler.add(MessageEvent, message=TextMessage)
-def noti_message(event):			
+			
 	while True:
 		if(noti.notification() == 2):
 			line_bot_api.push_message(
