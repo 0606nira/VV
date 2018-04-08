b4dbc1b9a6c766bb3fd151c0dcc44d0ab934884f
0606nira <nirachorn66@gmail.com>
2018-04-08 16:37:37 +0700

    'v'

--- app.py
+++ app.py
@@ -165,7 +165,8 @@ def noti_message():
 			line_bot_api.push_message(
 				'U5db26ce3aad1c4d83691ea5d6992116a', 
 				TextSendMessage(text='Light Off when ' +timeat))
-		continue
+		continue	
+noti_message()
 	
 
 image_carousel_template_message1 = TemplateSendMessage(
