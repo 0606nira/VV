4465373fee93e0b38d37544b6951b04e2260dd48
0606nira <nirachorn66@gmail.com>
2018-04-09 21:46:29 +0700

    'v'

--- noti.py
+++ noti.py
@@ -11,17 +11,17 @@ timeat = time.strftime('%A %d %B %Y, %H:%M:%S', timeis) # กำหนดรู�
 dummy = 315
 
 def notification():
-        global dummy
-        url = 'https://api.thingspeak.com/channels/455279/feeds.json?api_key=ZDDJL90IXYJOIQ3S&results=2'
-        response = urllib.request.urlopen(url)
-        data = json.load(response)
-        entry_status = data['feeds'][1]['entry_id']
-        status = data['feeds'][1]['field1']
-        if(entry_status == dummy + 1):
-			dummy = int(entry_status)
-			return status
-        else:
-			pass
-        print (dummy)
-        print (entry_status)
+	global dummy
+	url = 'https://api.thingspeak.com/channels/455279/feeds.json?api_key=ZDDJL90IXYJOIQ3S&results=2'
+	response = urllib.request.urlopen(url)
+	data = json.load(response)
+	entry_status = data['feeds'][1]['entry_id']
+	status = data['feeds'][1]['field1']
+	if(entry_status == dummy + 1):
+		dummy = int(entry_status)
+		return status
+	else:
+		pass
+	print ('dummy: ', dummy)
+	print ('entry: ', entry_status)
 		
