88969a7fccba9adef5c761072ab1f29675c636f2
0606nira <nirachorn66@gmail.com>
2018-04-10 17:06:06 +0700

    'v'

--- noti.py
+++ noti.py
@@ -8,31 +8,29 @@ import http.client, urllib
 timeis = time.localtime()
 timeat = time.strftime('%A %d %B %Y, %H:%M:%S', timeis) # กำหนดรูปแบบเวลา
 
-dummy = 315
+dummy = 322
 
 def notification():
-	global dummy
-	url = 'https://api.thingspeak.com/channels/455279/feeds.json?api_key=ZDDJL90IXYJOIQ3S&results=2'
-	response = urllib.request.urlopen(url)
-	data = json.load(response)
-	entry_status = data['feeds'][1]['entry_id']
-	status = data['feeds'][1]['field1']
-	if(entry_status == dummy + 1):
-		dummy = int(entry_status)
-		return status
-	else:
-		pass
-	print ('dummy: ', dummy)
-	print ('entry: ', entry_status)
+        global i, light1_detect_int, dummy
+        last_detect = datetime.datetime.now()
+        url = 'https://api.thingspeak.com/channels/455279/feeds.json?api_key=ZDDJL90IXYJOIQ3S&results=2'
+        response = urllib.request.urlopen(url)
+        data = json.load(response)
+        entry_status = data['feeds'][1]['entry_id']
+        be_status1 = data['feeds'][0]['field1']
+        be_status2 = data['feeds'][0]['field2']
+        last_status1 = data['feeds'][1]['field1']
+        last_status2 = data['feeds'][1]['field2']
+        if(entry_status != dummy):
+                if(last_status1 != be_status1):
+                        dummy = entry_status
+                        light = 1
+                        return last_status1, light
+                else:
+                        dummy = entry_status
+                        light = 2
+                        return last_status2, light
+        else:
+                pass
+        print (entry_status)
 		
-def noti_message():
-	while True:
-		if(notification() == 'On'):
-			line_bot_api.push_message(
-				'U5db26ce3aad1c4d83691ea5d6992116a', 
-				TextSendMessage(text='Light On when ' +timeat))
-		elif(notification() == 'Off'):
-			line_bot_api.push_message(
-				'U5db26ce3aad1c4d83691ea5d6992116a', 
-				TextSendMessage(text='Light Off when ' +timeat))
-		time.sleep(10)
