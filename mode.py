b5c725579fe7b715aae70f97c150d089745c59a4
0606nira <nirachorn66@gmail.com>
2018-04-14 17:13:52 +0700

    'v'

--- mode.py
+++ mode.py
@@ -8,24 +8,24 @@ def mode(mode):
 	params = urllib.parse.urlencode(
 		{'field1': mode, 
 		'key': API_KEY_WRITE} )
-    headers = { "Content-Type": "application/x-www-form-urlencoded",
-                "Accept": "text/plain" }
-    conn = http.client.HTTPConnection("api.thingspeak.com:80")
-    try:
-        conn.request( "POST", "/update", params, headers ) # send HTTP request
-        resp = conn.getresponse() # get HTTP response
-        #print ('status:', resp.status, resp.reason) # read HTTP status
-        entry_id = resp.read()  # read response string
+	headers = { "Content-Type": "application/x-www-form-urlencoded",
+		"Accept": "text/plain" }
+	conn = http.client.HTTPConnection("api.thingspeak.com:80")
+	try:
+		conn.request( "POST", "/update", params, headers ) # send HTTP request
+		resp = conn.getresponse() # get HTTP response
+		#print ('status:', resp.status, resp.reason) # read HTTP status
+		entry_id = resp.read()  # read response string
 		field1 = resp.read()
-        conn.close()            # close HTTP connection
-        if entry_id.isdigit() and int(entry_id) > 0:
-            print ('Entry ID:', entry_id)
+		conn.close()            # close HTTP connection
+		if entry_id.isdigit() and int(entry_id) > 0:
+			print ('Entry ID:', entry_id)
 			print ('mode is: ', fielse1)
-            return True
-        else:
-            return False
-    except:
-        print ("connection failed", sys.exc_info())
+			return True
+		else:
+			return False
+	except:
+		print ("connection failed", sys.exc_info())
 
 			#mode auto = 1
 			#mode manual = 0
