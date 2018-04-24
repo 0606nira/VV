af662eea09279aa45558289279068f2a31ba76fb
0606nira <nirachorn66@gmail.com>
2018-04-24 20:05:28 +0700

    'v'

--- app.py
+++ app.py
@@ -253,7 +253,7 @@ image_carousel_template_message3 = TemplateSendMessage(
 					)
 			),
 			ImageCarouselColumn(
-				image_url='http://campus.campus-star.com/app/uploads/2016/05/%E0%B8%A5%E0%B8%AD%E0%B8%94%E0%B8%99%E0%B9%87%E0%B8%AD%E0%B8%95%E0%B8%AD%E0%B8%B1%E0%B8%99%E0%B8%99%E0%B8%B5%E0%B9%89%E0%B9%80%E0%B8%82%E0%B8%B2%E0%B8%A7%E0%B9%88%E0%B8%B2%E0%B8%88%E0%B8%B0%E0%B9%84%E0%B8%94%E0%B9%89%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%9B%E0%B9%87%E0%B8%99%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%81-EG-%E0%B8%99%E0%B8%B0.jpg',
+				image_url='https://www.eg.mahidol.ac.th/egmu/images/egmu.jpg',
 				action=MessageTemplateAction(
 					label='Mahidol',
 					text='Mahidol',								
